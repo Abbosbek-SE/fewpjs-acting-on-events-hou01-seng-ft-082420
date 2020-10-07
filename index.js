9b11a3a2a71dc1f6b27bc3bbf76dbe5f026e3540
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-10-07 15:53:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+
+let dodger = document.getElementByID("dodger");
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else {
+        moveDodgerRight();
+    }
+
+})
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
